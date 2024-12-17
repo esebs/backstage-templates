51beb674cbdb358c6ba9a3a1c5703f001eb77ede
sescobar <sescobar@gbm.net>
2024-12-17 08:28:24 -0600

    test 2

--- quarkus-template/skeleton/src/main/java/__package__/application/interfaces/inbound/ProcessRequest.java
+++
@@ -1,8 +0,0 @@
-package {{ parameters.package }}.application.interfaces.inbound;
-
-import {{ parameters.package }}.infrastructure.inbound.rest.dto.request.RequestDTO;
-import {{ parameters.package }}.infrastructure.inbound.rest.dto.response.ResponseDTO;
-
-public interface ProcessRequest {
-    ResponseDTO process(RequestDTO requestDTO);
-}
--- quarkus-template/skeleton/src/main/java/__package__/application/interfaces/outbound/GetData.java
+++
@@ -1,7 +0,0 @@
-package {{ parameters.package }}.application.interfaces.outbound;
-
-import {{ parameters.package }}.infrastructure.inbound.rest.dto.request.RequestDTO;
-
-public interface GetData {
-    boolean get(RequestDTO requestDTO);
-}
